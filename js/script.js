f3bc12ae8b50152941255017efb8a04844190da5
Vyacheslav <parselian.study@yandex.ru>
2020-06-04 11:30:41 +0300

    подключил Я.карту

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
     playerWrap.addEventListener('click', e => {
       const target = e.target;
 
-      if (target.matches('.video__mediaplayer-playbutton') && player.paused) {
+      if (target === playerBtn && player.paused) {
         player.play();
         playerBtn.style.display = 'none';
         player.style.cursor = 'pointer';
@@ -19,9 +19,40 @@ window.addEventListener('DOMContentLoaded', () => {
         playerBtn.style.display = 'unset';
         player.style.cursor = 'unset';
       }
-
-      console.dir(player);
     });
   };
   controlVideoplayer();
+
+  const mapInit = () => {
+    ymaps.ready(init);
+
+    function init () {
+      let myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        center: [55.684183, 37.412783],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 15
+      });
+
+      let mapPlacemark = new ymaps.GeoObject({
+        geometry: {
+          type: 'Point',
+          coordinates: [55.684183, 37.412783]
+        }
+      });
+
+      myMap.geoObjects.add(mapPlacemark);
+      myMap.controls.remove('geolocationControl');
+      myMap.controls.remove('searchControl');
+      myMap.controls.remove('trafficControl');
+      myMap.controls.remove('typeSelector');
+      myMap.controls.remove('fullscreenControl');
+      myMap.controls.remove('zoomControl');
+      myMap.controls.remove('routeButtonControl');
+      myMap.controls.remove('rulerControl');
+    }
+  }
+  mapInit();
 });
