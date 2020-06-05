a17a88385a814417fd1764a692037b95a651f11c
jells <jells@jells.jells>
2020-06-05 20:14:15 +0300

    сделал слайдер-карусель для секции "Проекты", реализовал плавный скролл и оживил бургер-меню

--- js/script.js
+++ js/script.js
@@ -2,6 +2,74 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+  const toggleBurgerMenu = () => {
+    const burger = document.querySelector('.burger'),
+          burgerBtn = burger.querySelector('.burger__button');
+
+    burger.addEventListener('click', (e) => {
+      const target = e.target;
+
+      if (target.closest('.burger__button') || target.closest('.burger__menu-link')) {
+        burger.classList.toggle('burger_show');
+        burgerBtn.classList.toggle('burger__button_show');
+      }
+    });
+  };
+  toggleBurgerMenu();
+
+  const smoothAnchorScroll = () => {
+    const anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+    anchorLinks.forEach(anchor => {
+      anchor.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  };
+  smoothAnchorScroll();
+
+  const sliderCarousel = (wrap) => {
+    const slider = document.querySelector(wrap),
+          sliderWrap = slider.querySelector('.slider__wrap'),
+          slides = sliderWrap.querySelectorAll('img');
+
+    let counter = 0;
+
+    slider.addEventListener('click', (e) => {
+      const target = e.target,
+            scrolledWidth = slides[0].getBoundingClientRect().width + 30;
+
+
+      if (target.closest('.slider__arrow_right')) {
+        if (counter < slides.length - 3) {
+          counter++;
+
+          sliderWrap.style.transform = `translateX(-${scrolledWidth * counter}px)`;
+        } else if (counter === slides.length - 3) {
+          counter = 0;
+          sliderWrap.style.transform = 'translateX(0)';
+        }
+      } else if (target.closest('.slider__arrow_left')) {
+        if (counter > 0) {
+          counter--;
+
+          sliderWrap.style.transform = `translateX(${scrolledWidth * counter}px)`;
+        } else if (counter === 0) {
+          counter = slides.length - 3;
+          sliderWrap.style.transform = `translateX(-${scrolledWidth * counter}px)`;
+        }
+      }
+      console.log(counter);
+    })
+    
+  };
+  sliderCarousel('.projects-slider');
+  sliderCarousel('.projects-slider_2');
+
   const controlVideoplayer = () => {
     const playerWrap = document.querySelector('.video__mediaplayer'),
         player = playerWrap.querySelector('video'),
@@ -22,6 +90,20 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
   controlVideoplayer();
+
+  const activateAccordeon = () => {
+    const faqWrap = document.querySelector('.faq__wrap');
+
+    faqWrap.addEventListener('click', (e) => {
+      const target = e.target;
+
+      if (target.closest('.faq__wrap-question')) {
+        target.nextElementSibling.classList.toggle('faq__wrap-answer_show');
+      }
+    });
+  };
+  activateAccordeon();
+
 
   const mapInit = () => {
 
