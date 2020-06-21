0f27c812150684e6b0ba4e52aa829543149c0c94
jells <jells@jells.jells>
2020-06-21 16:54:17 +0300

    оптимизировал проект под pagespeed(фон главного блока режет показатели на 15 баллов)

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   const disablePreloader = () => {
-    document.querySelector('.preloader').classList.remove('preloader_visible');
+    setTimeout(() => {
+      document.querySelector('.preloader').classList.remove('preloader_visible');
+    }, 1000);
   };
   disablePreloader();
 
@@ -349,43 +351,4 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
   togglePopup();
-
-  const mapInit = () => {
-
-    // ymaps.ready(init);
-
-    function init () {
-      let myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «широта, долгота».
-        center: [55.684183, 37.412783],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 15
-      });
-
-      let mapPlacemark = new ymaps.GeoObject({
-        geometry: {
-          type: 'Point',
-          coordinates: [55.684183, 37.412783]
-        }
-      });
-
-      myMap.geoObjects.add(mapPlacemark);
-      myMap.controls.remove('geolocationControl');
-      myMap.controls.remove('searchControl');
-      myMap.controls.remove('trafficControl');
-      myMap.controls.remove('typeSelector');
-      myMap.controls.remove('fullscreenControl');
-      myMap.controls.remove('zoomControl');
-      myMap.controls.remove('routeButtonControl');
-      myMap.controls.remove('rulerControl');
-
-      myMap.behaviors.disable(['drag']);
-    }
-    init();
-
-
-  };
-  mapInit();
 });
